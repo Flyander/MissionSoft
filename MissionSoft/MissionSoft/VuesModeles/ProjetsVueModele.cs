438161e319e45e88b31a9282d5ea625850c1d7b1
gregoire hamon <gregoire.22@hotmail.fr>
2019-11-18 16:14:05 +0100

    La liste elle s'affiche

--- MissionSoft/MissionSoft/VuesModeles/ProjetsVueModele.cs
+++ MissionSoft/MissionSoft/VuesModeles/ProjetsVueModele.cs
@@ -12,7 +12,6 @@ namespace MissionSoft.VuesModeles
     {
         #region Attributs
 
-        private string _nomProjet;
         private ObservableCollection<Projet> _maListeProjets;
 
         #endregion
@@ -27,11 +26,33 @@ namespace MissionSoft.VuesModeles
             DateTime dateFin = new DateTime(01 / 02 / 2019);
 
             Projet Argo = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo1 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo2 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo3 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo4 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo5 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo6 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo7 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo8 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo9 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo10 = new Projet("Argo", dateDebut, dateFin, 1000);
+            Projet Argo11 = new Projet("Argo", dateDebut, dateFin, 1000);
 
-            _maListeProjets.Add(Argo);
+            MaListeProjets.Add(Argo);
+            MaListeProjets.Add(Argo1);
+            MaListeProjets.Add(Argo2);
+            MaListeProjets.Add(Argo3);
+            MaListeProjets.Add(Argo4);
+            MaListeProjets.Add(Argo5);
+            MaListeProjets.Add(Argo6);
+            MaListeProjets.Add(Argo7);
+            MaListeProjets.Add(Argo8);
+            MaListeProjets.Add(Argo9);
+            MaListeProjets.Add(Argo10);
+            MaListeProjets.Add(Argo11);
 
-            _nomProjet = "test";
         }
+
         #endregion
 
         #region Getters/Setters
@@ -47,20 +68,6 @@ namespace MissionSoft.VuesModeles
             {
                 _maListeProjets = value;
                 OnPropertyChanged(nameof(MaListeProjets));
-            }
-        }
-
-        public string NomProjet
-        {
-            get
-            {
-                return _nomProjet;
-            }
-
-            set
-            {
-                _nomProjet = value;
-                OnPropertyChanged(nameof(NomProjet));
             }
         }
 
