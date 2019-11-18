8251abac77271c60705b7800391c53b4656001f9
YohannFin <yohann.fin13@gmail.com>
2019-11-18 11:49:02 +0100

    Ajout region à la classe mission

--- MissionSoft/MissionSoft/Modeles/Mission.cs
+++ MissionSoft/MissionSoft/Modeles/Mission.cs
@@ -7,6 +7,7 @@ namespace MissionSoft.Modeles
 {
     class Mission
     {
+        #region Attributs
         public static ArrayList CollClassMission = new ArrayList();
 
         private string _nom;
@@ -14,7 +15,9 @@ namespace MissionSoft.Modeles
         private int _nbHeuresPrevues;
         private Dictionary<DateTime, int> _releveHoraire = new Dictionary<DateTime, int>();
         private Intervenant _executant;
+        #endregion
 
+        #region Constructeurs
         public Mission(string nom, string description, int nbHeuresPrevues, Dictionary<DateTime, int> releveHoraire, Intervenant executant)
         {
             _nom = nom;
@@ -24,11 +27,17 @@ namespace MissionSoft.Modeles
             _executant = executant;
             CollClassMission.Add(this);
         }
+        #endregion
 
+        #region Getters/Setters
         public string Nom { get => _nom; set => _nom = value; }
         public string Description { get => _description; set => _description = value; }
         public int NbHeuresPrevues { get => _nbHeuresPrevues; set => _nbHeuresPrevues = value; }
         public Dictionary<DateTime, int> ReleveHoraire { get => _releveHoraire; set => _releveHoraire = value; }
         internal Intervenant Executant { get => _executant; set => _executant = value; }
+        #endregion
+
+        #region Methodes
+        #endregion
     }
 }
